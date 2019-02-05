61407846da3caf5ec3743d6e515a54d119970329
Anton Rassadin <toha90@gmail.com>
2019-02-05 18:46:16 +0300

    ZEN-102 made GetMRGSProxy public

--- Source/PsMRGS/Classes/PsMRGSLibrary.h
+++ Source/PsMRGS/Classes/PsMRGSLibrary.h
@@ -13,6 +13,7 @@ class PSMRGS_API UPsMRGSLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
+public:
 	/** Get MRGS proxy object */
 	UFUNCTION(BlueprintCallable, Category = MRGS, BlueprintPure)
 	static UPsMRGSProxy* GetMRGSProxy();
